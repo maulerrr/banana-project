43c7669c4ef1261c6105f94249b8954bd801ee9d
maulerrr <211239@astanait.edu.kz>
2023-06-16 12:27:50 +0600

    deployment: remote rabbit

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,11 @@
 package config
 
 var (
-	Port  = ":3000"
-	DBUrl = "postgres://maulerrr:esF25OkWzbga@ep-red-frog-609625.us-east-2.aws.neon.tech/banana"
-	//DBUrl         = "postgres://maulerrr:1111@localhost:5432/banana"
+	Port          = ":3000"
+	DBUrl         = "postgres://maulerrr:esF25OkWzbga@ep-red-frog-609625.us-east-2.aws.neon.tech/banana"
 	AuthSvcUrl    = ":50051"
 	PostSvcUrl    = ":50052"
 	CommentSvcUrl = ":50053"
 	JWTSecretKey  = "nothing ever goes as planned in this cursed world"
-	AMPQUrl       = "amqp://guest:guest@0.0.0.0:5672/"
+	AMPQUrl       = "amqp://guest:guest@rabbitmq:5672/"
 )
