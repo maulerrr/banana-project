3436c8b072bbad6ff1b3c05703781056505b1987
maulerrr <211239@astanait.edu.kz>
2023-06-16 11:47:56 +0600

    deployment: remote postgres

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,9 @@
 package config
 
 var (
-	Port = ":3000"
-	//DBUrl         = "postgres://maulerrr:esF25OkWzbga@ep-red-frog-609625.us-east-2.aws.neon.tech/banana"
-	DBUrl         = "postgres://maulerrr:1111@localhost:5432/banana"
+	Port  = ":3000"
+	DBUrl = "postgres://maulerrr:esF25OkWzbga@ep-red-frog-609625.us-east-2.aws.neon.tech/banana"
+	//DBUrl         = "postgres://maulerrr:1111@localhost:5432/banana"
 	AuthSvcUrl    = ":50051"
 	PostSvcUrl    = ":50052"
 	CommentSvcUrl = ":50053"
